b5898e733611b8f699a3b4aebaba1ad1ba8a9c5e
BarracudaPff <mrm.kikll@gmail.com>
2019-04-17 00:34:52 +0300

    register post's service and fetcher in koin module

--- src/com/apmath/template/infrastructure/ServiceManager.kt
+++ src/com/apmath/template/infrastructure/ServiceManager.kt
@@ -1,7 +1,13 @@
 package com.apmath.template.infrastructure
 
+import com.apmath.template.domain.fetchers.PostsFetcherInterface
+import com.apmath.template.domain.services.PostsService
+import com.apmath.template.domain.services.PostsServiceInterface
+import com.apmath.template.infrastructure.fetchers.PostsFetcher
 import io.ktor.application.Application
 import org.koin.dsl.module
+import org.koin.experimental.builder.singleBy
+import org.koin.ktor.ext.inject
 
 /**
  * Service Manager for services
@@ -16,6 +22,7 @@ import org.koin.dsl.module
  * ```
  */
 class ServiceManager(app: Application) {
+    val postsService by app.inject<PostsFetcherInterface>()
 
     companion object {
         private var instance: ServiceManager? = null
@@ -44,6 +51,8 @@ class ServiceManager(app: Application) {
          * Modules in which services connect to their interfaces
          */
         val serviceManagerModule = module {
+            singleBy<PostsServiceInterface, PostsService>()
+            singleBy<PostsFetcherInterface, PostsFetcher>()
         }
     }
 }

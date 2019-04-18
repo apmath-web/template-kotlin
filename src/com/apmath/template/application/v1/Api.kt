c2a0852354a4a502d2a263c0c32967c79ccba61b
BarracudaPff <mrm.kikll@gmail.com>
2019-04-18 19:26:15 +0300

    add import

--- src/com/apmath/template/application/v1/Api.kt
+++ src/com/apmath/template/application/v1/Api.kt
@@ -7,6 +7,7 @@ import io.ktor.application.call
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.route
+import org.koin.ktor.ext.inject
 
 internal fun Routing.v1() {
 

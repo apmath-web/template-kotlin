bebc6f36cf4f173bbd89508bc24e6855c94d88e8
malinink <apmath@malinink.ru>
2019-10-15 22:37:21 +0300

    Bump ktor version to 1.3.0-beta-1, refuse of kotlinx.io, prefer local packages

--- test/com/apmath/template/ApplicationTest.kt
+++ test/com/apmath/template/ApplicationTest.kt
@@ -8,34 +8,30 @@ import io.ktor.client.engine.mock.respondError
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.headersOf
-import kotlinx.coroutines.io.ByteReadChannel
-import kotlinx.coroutines.runBlocking
+import io.ktor.utils.io.ByteReadChannel
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     // TODO example of complex test with client mock
-    fun testClientMock() {
-        runBlocking {
-            val client = HttpClient(MockEngine) {
-                engine {
-                    addHandler { request ->
-                        when (request.url.encodedPath) {
-                            "/" -> respond(
-                                ByteReadChannel(byteArrayOf(1, 2, 3)),
-                                HttpStatusCode.OK,
-                                headersOf("X-MyHeader", "MyValue")
-                            )
-                            else -> respondError(HttpStatusCode.NotFound, "Not Found ${request.url.encodedPath}")
-                        }
-
+    suspend fun testClientMock() {
+        val client = HttpClient(MockEngine) {
+            engine {
+                addHandler { request ->
+                    when (request.url.encodedPath) {
+                        "/" -> respond(
+                            ByteReadChannel(byteArrayOf(1, 2, 3)),
+                            HttpStatusCode.OK,
+                            headersOf("X-MyHeader", "MyValue")
+                        )
+                        else -> respondError(HttpStatusCode.NotFound, "Not Found ${request.url.encodedPath}")
                     }
                 }
             }
-
-            assertEquals(byteArrayOf(1, 2, 3).toList(), client.get<ByteArray>("/").toList())
-            assertEquals("MyValue", client.call("/").response.headers["X-MyHeader"])
-            assertEquals("Not Found other/path", client.get<String>("/other/path"))
         }
+
+        assertEquals(byteArrayOf(1, 2, 3).toList(), client.get<ByteArray>("/").toList())
+        assertEquals("MyValue", client.call("/").response.headers["X-MyHeader"])
+        assertEquals("Not Found other/path", client.get<String>("/other/path"))
     }
 }

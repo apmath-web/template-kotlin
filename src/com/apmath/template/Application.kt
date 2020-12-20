f259b99683213536258fe76223ed0fa6c42bd575
Константин Малинин <km@cm.expert>
2020-12-20 22:16:20 +0300

    update lib versions to latest

--- src/com/apmath/template/Application.kt
+++ src/com/apmath/template/Application.kt
@@ -9,7 +9,7 @@ import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.locations.Locations
 import io.ktor.routing.Routing
-import org.koin.Logger.slf4jLogger
+import org.koin.logger.slf4jLogger
 import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
